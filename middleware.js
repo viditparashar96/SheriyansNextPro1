c34016fe16e27def8e5aaa2cca6c9b53d6119fd7
vidit <viditsharma818@gmail.com>
2023-09-03 22:11:30 +0530

    remove middlewares

--- middleware.js
+++ middleware.js
@@ -2,17 +2,17 @@ import { NextResponse } from 'next/server'
  
 
 export function middleware(req) {
-    const path=req.nextUrl.pathname
-    const isPublicPath=path === '/login' || path==='/signup'
-    const token=req.cookies.get('token')
-    if(isPublicPath && token){
-      console.log(token)
-        return NextResponse.redirect(new URL('/',req.nextUrl))
-    }
-    if(!isPublicPath && !token){
-        return NextResponse.redirect(new URL('/login',req.nextUrl))
+    // const path=req.nextUrl.pathname
+    // const isPublicPath=path === '/login' || path==='/signup'
+    // const token=req.cookies.get('token')
+    // if(isPublicPath && token){
+    //   console.log(token)
+    //     return NextResponse.redirect(new URL('/',req.nextUrl))
+    // }
+    // if(!isPublicPath && !token){
+    //     return NextResponse.redirect(new URL('/login',req.nextUrl))
 
-    }
+    // }
 }
  
 
