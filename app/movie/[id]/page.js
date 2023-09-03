ddcacc757252c4333e1eeba52b44932ec8e4cdb5
vidit <viditsharma818@gmail.com>
2023-09-03 13:48:46 +0530

    loading added

--- app/movie/[id]/page.js
+++ app/movie/[id]/page.js
@@ -4,14 +4,16 @@ import Showhero from '@/components/Showhero'
 import React, { useEffect, useState } from 'react'
 import Cast from '@/components/cast'
 import axios from 'axios'
+import Loading from '@/components/Loading'
 function page({params}) {
     const [showDets,setShowDets]=useState({})
+    const [loading,setLoading]=useState(true)
     console.log("movie page is here")
-
-        console.log(showDets)
     async function fecthshowdets(){
+        setLoading(true)
         const res=await axios.get(`https://api.themoviedb.org/3/movie/${params.id}?api_key=8fa1d7ebd4f44371dfeba46c72f4153a`)
         // console.log(res.data)
+        setLoading(false)
         setShowDets(res.data)
     }
     useEffect(()=>{
@@ -21,7 +23,10 @@ function page({params}) {
     <div>
         <Navbar/>
         {/* Showhero is for movie */}
-        <Showhero showDets={showDets}/> 
+        {
+            loading?(<Loading/>):(<Showhero showDets={showDets}/> )
+        }
+        
         <Cast/>
         </div>
   )
--- app/tv/[id]/page.js
+++ app/tv/[id]/page.js
@@ -4,13 +4,19 @@ import ShowheroTv from '@/components/ShowheroTv'
 import React, { useEffect, useState } from 'react'
 import Cast from '@/components/cast'
 import axios from 'axios'
+import Loading from '@/components/Loading'
 function page({params}) {
     const [showDets,setShowDets]=useState({})
+    const [loading,setLoading]=useState(true)
+
     console.log("tv page is here")
         console.log(showDets)
     async function fecthshowdets(){
+        setLoading(true)
         const res=await axios.get(`https://api.themoviedb.org/3/tv/${params.id}?api_key=8fa1d7ebd4f44371dfeba46c72f4153a`)
         // console.log(res.data)
+        setLoading(false)
+
         setShowDets(res.data)
     }
     useEffect(()=>{
@@ -19,7 +25,10 @@ function page({params}) {
   return (
     <div>
         <Navbar/>
-        <ShowheroTv showDets={showDets}/>
+        {
+            loading?(<Loading/>):(<ShowheroTv showDets={showDets}/> )
+        }
+        
         <Cast/>
         </div>
   )
